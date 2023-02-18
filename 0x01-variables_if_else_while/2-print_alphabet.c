cccbd3a21d63d9ad5c445504c364d375838d1540
79Didman <dmaniratanga@yahoo.fr>
2023-02-17 19:51:49 -0800

    display last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - sort alphabet 
+ *
+ * Return: 0 (succes)
+ */
+
+int main(void)
+{
+	for ( char c = 'a',c<='z', c++)
+	{
+		putchar(c,'\n');
+	}
+	return (0);
+
+}
