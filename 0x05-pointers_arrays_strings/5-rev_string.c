ad373e7470f9f6712c4624c7ccb1feaf72915ff3
79Didman <dmaniratanga@yahoo.fr>
2023-03-01 12:44:47 -0800

    5 rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse , followed by a new line
+ * @s: the string to be printed
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		i = j - i;
+		s[n] = s[i];
+	}
+	
+
+}
+
