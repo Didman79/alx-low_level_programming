12092e493f26bf67dc7ace8d3ecebbc7f63d2692
79Didman <dmaniratanga@yahoo.fr>
2023-03-01 04:40:19 -0800

    0 concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat- concatenate value of *src to the value of *dest
+ * @*dest : where concatenate will be to
+ * @*src : where concatenate will be from
+ * return :*dest
+ */
+
+
+
+Void char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; i != '\0'; i++)
+	{
+		while (j != '\0')
+			*dest[i] = *src[j];
+		j++;
+		i++;
+	}
+	*dest[i] = "\0";
+	return (*dest);
+}
