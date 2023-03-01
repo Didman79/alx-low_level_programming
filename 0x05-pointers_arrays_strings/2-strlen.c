cdeae0507a28e4a8cee4fc2d6ce9f67d81d22967
79Didman <dmaniratanga@yahoo.fr>
2023-03-01 10:19:19 -0800

    2 strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen- to evaluate the length of a string
+ * @s: string to determine length
+ * return: length
+ *
+ */
+
+
+void int _strlen(char *s)
+{
+	int i = 0;
+	int n = 0;
+
+	for (s[i] != '\0'; i++)
+	{
+		n++;
+	}
+
+	return (n);
+}
