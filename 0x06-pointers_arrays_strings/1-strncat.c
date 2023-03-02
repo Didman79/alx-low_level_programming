f40607e239a54ec3cccd62e70b732fe161e2f5ba
79Didman <dmaniratanga@yahoo.fr>
2023-03-02 10:11:29 -0800

    1 strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates a string src to a string dest
+ * @dest: string to concatenate to
+ * @src: string to concatenate from
+ * @n: number of bytes to be appended form src to dest
+ * Return: dest after concatanation
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
