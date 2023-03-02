c158469952d2b05178a59fe76cc149a5eee362a7
79Didman <dmaniratanga@yahoo.fr>
2023-03-02 12:03:10 -0800

    2 strncpy.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,14 +15,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 
 	while (src[i] != '\0' && i < n)
-	{
 		dest[i++] = src[i];
-	}
 
 	while (i < n)
-	{
 		dest[i++] = '\0';
-	}
 
 	return (dest);
 }
