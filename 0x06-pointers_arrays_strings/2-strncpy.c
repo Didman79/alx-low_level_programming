7e0b465f339287a80acd97edb246d0dae497db83
79Didman <dmaniratanga@yahoo.fr>
2023-03-02 11:15:07 -0800

    2 strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates a string src to a string dest
+ * @dest: string to concatenate to
+ * @src: string to concatenate from
+ * @n: number of bytes to be appended form src to dest
+ * Return: dest after concatanation
+ */
+
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	for (i= 0; s[i]&&i < n; s[i] != '\n'; i++)
+		dest[l] = src[i];
+	dest[i] = '\n';
+
+	return (dest);
+}
