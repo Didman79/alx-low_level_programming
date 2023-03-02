16538adfd1c5b36ecb6167b0515c5c87e61f551d
79Didman <dmaniratanga@yahoo.fr>
2023-03-02 12:36:45 -0800

    3 strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,11 @@
  * @s1: first string to compare
  * @s2: second string to compare
  *
- * Return: less than 0 if s1 is less than s2  for the first different byte, 
+ * Return:less than 0 if s1 is less than s2  for the first different byte,
  * 0 if they're equal,
  * and more than 0 if s1 is greater than s2 for the first different byte
  *
  */
-
 
 int _strcmp(char *s1, char *s2)
 {
