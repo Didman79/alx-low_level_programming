3d8cd55a1b027a824d8b3e2c9c17d551f9588eb0
79Didman <dmaniratanga@yahoo.fr>
2023-03-02 14:14:27 -0800

    6 cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes every letter of a string
+ * @str: string to modify
+ *
+ * Return: the string capitalized
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+
+	char specials[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == speciales[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (str);
+}
