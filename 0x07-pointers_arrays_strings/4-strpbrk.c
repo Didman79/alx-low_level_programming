6477125029f1876843bc685e8c0a47a8c0521b0e
79Didman <dmaniratanga@yahoo.fr>
2023-03-06 13:35:45 -0800

    4 strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - matches character in string s from string accept
+ * @s: where to match characters
+ * @accept: contains charaters to match in s
+ *
+ * Return: the first character matched
+ * NULL if no matching occured
+ *
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	
+	while (*s)
+	{
+		for (i = 0, accept[i]; i++)
+		{
+			if ( s[i] == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
+			
