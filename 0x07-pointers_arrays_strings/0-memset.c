3c24b888804a47cb9fa65116023f79b27722760b
79Didman <dmaniratanga@yahoo.fr>
2023-03-06 08:02:31 -0800

    0 memset .c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  * *_memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
  * @s: the string  where to rwite
  * @b: the constant byte to write in s
+ * @n: n times b gonna be written in s
  *
  * Returns: the  pointer S after writing in its memory area
  */
@@ -14,7 +15,7 @@ char *_memset(char *s, char b, unsigned int n)
 	char b;
 	unsigned int i;
 
-	while (i = 0; i <= n)
+	while (i = 0; i < n)
 	{
 		s[i] = b;
 		i++;
