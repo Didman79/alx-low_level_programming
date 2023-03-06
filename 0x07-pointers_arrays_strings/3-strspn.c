ff5de810c02e1df8a20e93748093864fb35dec98
79Didman <dmaniratanga@yahoo.fr>
2023-03-06 12:35:54 -0800

    3 strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,7 +15,8 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept != '\0'; j++)
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
@@ -24,7 +25,9 @@ unsigned int _strspn(char *s, char *accept)
 			}
 		}
 		if (flag == 0)
+		{
 			return (k);
+		}
 	}
 	return (NULL);
 }
